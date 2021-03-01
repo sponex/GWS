cec97be17507a828d4a52d59cdff787f0090c8bb
Simon Whitehouse <simon.whitehouse@live.co.uk>
2021-03-01 14:39:39 +0000

    first version commited

---
+++ gws.go
@@ -0,0 +1,32 @@
+// Tool     -   GWS
+// Author   -   Simon Whitehouse
+// Version  -   0.1
+// Description  - GWS also known as GoWebServer, My own version of pythons simpleHTTP server so I can serve files.
+
+package main
+
+import (
+	"log" //Logging package
+	"net/http" //Networking package
+    "flag" //Comand line flag package
+)
+
+func main() {
+
+// Define Commands Line options
+// port for ports
+// dir for directorys
+port := flag.String("p", ":8000", "HTTP Port to use (Defaults to 8000)")
+//dir := flag.string("d", ".", "Set Directory to Serve (Defaults to current)")
+flag.Parse()
+
+mux := http.NewServeMux()
+fileServer := http.FileServer(http.Dir("."))
+mux.Handle("/", http.StripPrefix("/", fileServer))
+
+
+// Logging/ Outpt
+log.Printf("Server started on Port %s", *port)
+err := http.ListenAndServe(*port, mux)
+log.Fatal(err)
+}
