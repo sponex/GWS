e8a2cd34c88f123647321a263d6ea23cdcf7660d
Simon Whitehouse <simon.whitehouse@live.co.uk>
2021-03-01 15:38:58 +0000

    removed need for colon specification at command line

--- gws.go
+++ gws.go
@@ -1,6 +1,6 @@
 // Tool     -   GWS
 // Author   -   Simon Whitehouse
-// Version  -   0.1
+// Version  -   0.2
 // Description  - GWS also known as GoWebServer, My own version of pythons simpleHTTP server so I can serve files.
 
 package main
@@ -9,6 +9,7 @@ import (
 	"log" //Logging package
 	"net/http" //Networking package
     "flag" //Comand line flag package
+	//"fmt"
 )
 
 func main() {
@@ -17,6 +18,7 @@ func main() {
 // port for ports
 // dir for directorys
 port := flag.String("p", ":8000", "HTTP Port to use (Defaults to 8000)")
+
 //dir := flag.string("d", ".", "Set Directory to Serve (Defaults to current)")
 flag.Parse()
 
@@ -27,6 +29,6 @@ mux.Handle("/", http.StripPrefix("/", fileServer))
 
 // Logging/ Outpt
 log.Printf("Server started on Port %s", *port)
-err := http.ListenAndServe(*port, mux)
+err := http.ListenAndServe(":" + *port, mux)
 log.Fatal(err)
 }
