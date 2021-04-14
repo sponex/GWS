9740795c7ba7e160b260b75036f86faa97cc73b0
Simon Whitehouse <simon.whitehouse@live.co.uk>
2021-04-14 16:21:45 +0100

    Added SSL support

--- gws.go
+++ gws.go
@@ -1,7 +1,7 @@
 // Tool     -   GWS
 // Author   -   Simon Whitehouse
-// Version  -   0.2
-// Description  - GWS also known as GoWebServer, My own version of pythons simpleHTTP server so I can serve files.
+// Version  -   0.3
+// Description  - GWS also known as GoWebServer, My own version of Python's simpleHTTP server so I can serve files.
 
 package main
 
@@ -9,26 +9,31 @@ import (
 	"log" //Logging package
 	"net/http" //Networking package
     "flag" //Comand line flag package
-	//"fmt"
+	"fmt" // debugging
 )
 
 func main() {
 
 // Define Commands Line options
-// port for ports
-// dir for directorys
+// port for ports, dir for directorys, SSL to force use of SSL. 
 port := flag.String("p", "8000", "HTTP Port to use (Defaults to 8000)")
+dir := flag.String("d", ".", "Set Directory to Serve (Defaults to current)")
+ssl := flag.Bool("s", false, "Forces use of SSL (Defaults to false)")
 
-//dir := flag.string("d", ".", "Set Directory to Serve (Defaults to current)")
 flag.Parse()
 
 mux := http.NewServeMux()
-fileServer := http.FileServer(http.Dir("."))
+fileServer := http.FileServer(http.Dir(*dir))
 mux.Handle("/", http.StripPrefix("/", fileServer))
 
 
-// Logging/ Outpt
-log.Printf("Server started on Port %s", *port)
-err := http.ListenAndServe(":" + *port, mux)
-log.Fatal(err)
+if *ssl == false {
+	log.Printf("Server started on Port %s", *port)
+	err := http.ListenAndServe(":" + *port, mux)
+	log.Fatal(err)
+} else {
+	log.Printf("Server started on SSL Port %s", *port)
+	err := http.ListenAndServeTLS(":" + *port, "./tls/cert.pem", "./tls/key.pem", mux)
+	log.Fatal(err)
+}
 }
